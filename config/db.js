23174a6f7b9428173050116663f4798c253961ce
GoranYankov <goran415@abv.bg>
2017-12-30 18:36:32 +0200

    add server

--- config/db.js
+++ config/db.js
@@ -1,4 +1,7 @@
 module.exports = config = {
-    database: 'mongodb://localhost:27017/my-server-app',
+	database: 'mongodb://rusenez:goran415@ds135917.mlab.com:35917/test-db',
+   
     secret: 'GoranYankov'
 }
+
+// database2: 'mongodb://localhost:27017/my-server-app',
