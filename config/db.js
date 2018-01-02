81532e424115b533eb476dc3c09a7bd1fc7ca142
GoranYankov <goran415@abv.bg>
2018-01-02 16:09:03 +0200

    add

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 module.exports = config = {
-	database2: 'mongodb://localhost:27017/my-server-app',
-   database: 'mongodb://rusenez:rusenez@cluster0-shard-00-00-ks8md.mongodb.net:27017,cluster0-shard-00-01-ks8md.mongodb.net:27017,cluster0-shard-00-02-ks8md.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
+	database: 'mongodb://localhost:27017/my-server-app',
+   //database: 'mongodb://rusenez:rusenez@cluster0-shard-00-00-ks8md.mongodb.net:27017,cluster0-shard-00-01-ks8md.mongodb.net:27017,cluster0-shard-00-02-ks8md.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin',
     secret: 'GoranYankov'
 }
 
