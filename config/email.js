d8b71fb2cc2c1ee32d7c1178517103037f64ed01
GoranYankov <goran415@abv.bg>
2018-02-25 17:30:29 +0200

    edint email

--- config/email.js
+++ config/email.js
@@ -1,7 +1,7 @@
 module.exports = {
         host: 'smtp.abv.bg', port: 465, secure: true, // true for 465, false for other ports
         auth: {
-            user: 'magazin-zdrav@abv.bg', // generated ethereal user
+            user: 'magazin-zdrave-one@abv.bg', // generated ethereal user
             pass: 'goran415' // generated ethereal password
         }
 }
